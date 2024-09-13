124f7eac20e7c2570446f625463cb8c142ee85c9
Vic Mortelmans <vicmortelmans@gmail.com>
2024-09-13 20:08:16 +0200

    fix

--- get_news.sh
+++ get_news.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd /home/vic/raspberry-pi-feed-broadcast
 export GOOGLE_APPLICATION_CREDENTIALS="/home/vic/joyce-2d487b74673d.json"
-python3 get_news.py $@
+source env/bin/activate
+python get_news.py $@
